24dbf1f56223d822fdc37a8fbc82063693e426f2
khauvannam <nkhauvan@gmail.com>
2024-09-18 23:18:26 +0700

    add controller

---
+++ Product_API/Controllers/CommentController.cs
@@ -0,0 +1,61 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Product_API.Domains.Comments;
+using Product_API.Features.Comments;
+
+namespace Product_API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class CommentController(ISender sender) : ControllerBase
+{
+    [HttpPost]
+    public async Task<IActionResult> Add([FromBody] CreateComment.Command command)
+    {
+        if (await sender.Send(command) is { IsFailure: true } result)
+            return BadRequest(result.ErrorTypes);
+
+        return Ok();
+    }
+
+    [HttpDelete("{commentId:int}")]
+    public async Task<IActionResult> Delete(int commentId)
+    {
+        var command = new DeleteComment.Command(commentId);
+        if (await sender.Send(command) is { IsFailure: true } result)
+            return BadRequest(result.ErrorTypes);
+
+        return Ok();
+    }
+
+    [HttpPut("{commentId:int}")]
+    public async Task<IActionResult> Update(int commentId, [FromBody] UpdateCommentRequest request)
+    {
+        var command = new UpdateComment.Command(commentId, request);
+        var result = await sender.Send(command);
+        if (!result.IsFailure)
+            return Ok();
+
+        return NotFound(result.ErrorTypes);
+    }
+
+    [HttpGet("{commentId:int}")]
+    public async Task<IActionResult> GetById(int commentId)
+    {
+        var result = await sender.Send(new GetCommentById.Query(commentId));
+        if (!result.IsFailure)
+            return Ok(result.Value);
+
+        return NotFound(result.ErrorTypes);
+    }
+
+    [HttpGet("/product/{productId:guid}")]
+    public async Task<IActionResult> GetAll(Guid productId)
+    {
+        var result = await sender.Send(new GetAllComment.Query(productId));
+        if (!result.IsFailure)
+            return Ok(result.Value);
+
+        return NotFound(result.ErrorTypes);
+    }
+}
